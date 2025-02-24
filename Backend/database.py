8bf2119bb5b35f63b1e1bd8dbe883861e5216c46
at6132 <avitaub6@gmail.com>
2025-02-24 16:41:42 -0500

    Update database.py
    Added PostgreSQL Database link

--- Backend/database.py
+++ Backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
-DATABASE_URL = "sqlite:///./signups.db"  # Using SQLite for local testing
+DATABASE_URL = "postgresql://postgres:yGYmhOPxhCHuTbxJQUryxPxzqCeFqgBT@switchyard.proxy.rlwy.net:27573/railway"  # Using SQLite for local testing
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
